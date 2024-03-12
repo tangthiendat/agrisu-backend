17538a06b63696deaba00603f023b4581154b3f0
Tang Thien Dat <thiendattang@gmail.com>
2024-03-12 14:33:34 +0700

    init

---
+++ src/main/java/com/ttdat/agrisubackend/AgrisuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ttdat.agrisubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrisuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrisuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttdat/agrisubackend/AgrisuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttdat.agrisubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrisuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
